e12b0d63c5b03c6818e3452b53b6066bb75e5c72
Ricardocafee <ricardocafee00@gmail.com>
2025-02-16 22:28:47 +0000

    Project 2 done

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function showVideo() {
+    let videoFrame = document.getElementById("videoFrame");
+    videoFrame.src = "https://www.youtube.com/embed/y0weLQa5hrU"; // Your Video ID
+    document.getElementById("videoModal").style.display = "flex";
+}
+
+function closeVideo() {
+    let videoFrame = document.getElementById("videoFrame");
+    videoFrame.src = ""; // Stop the video when closing
+    document.getElementById("videoModal").style.display = "none";
+}
+
+// Ensure modal is hidden on page load
+    window.onload = function() {
+        document.getElementById("videoModal").style.display = "none";
+    };
