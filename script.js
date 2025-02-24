368a17f2121b40bf22a7e18e3ae35be0f9beab5c
Ricardocafee <ricardocafee00@gmail.com>
2025-02-24 21:52:01 +0000

    Portfolio completed

--- script.js
+++ script.js
@@ -11,13 +11,29 @@ function showVideo() {
     document.getElementById("videoModal").style.display = "flex";
 }
 
+function showVideo1() {
+    let videoFrame = document.getElementById("videoFrame1");
+    videoFrame.innerHTML = `<video width="80%" controls>
+                                <source src="./assets/booking_system.mp4" type="video/mp4">
+                                Your browser does not support the video tag.
+                            </video>`;
+    document.getElementById("videoModal1").style.display = "flex";
+}
+
 function closeVideo() {
     let videoFrame = document.getElementById("videoFrame");
     videoFrame.src = ""; // Stop the video when closing
     document.getElementById("videoModal").style.display = "none";
 }
 
+function closeVideo1() {
+    let videoFrame1 = document.getElementById("videoFrame1");
+    videoFrame1.src = ""; // Stop the video when closing
+    document.getElementById("videoModal1").style.display = "none";
+}
+
 // Ensure modal is hidden on page load
     window.onload = function() {
         document.getElementById("videoModal").style.display = "none";
+         document.getElementById("videoModal1").style.display = "none";
     };
